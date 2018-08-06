66f77de83b2c3329a2c394bcdaca91a485170aa4
Felipe <cezarfelipe@gmail.com>
2018-08-06 09:32:34 -0300

    Aula 00

---
+++ js/main.js
@@ -0,0 +1,29 @@
+{
+
+/* ========================
+    Inicialização Firebase
+======================== */
+
+var config = {
+    apiKey: "AIzaSyBl9KWZ0hW-rTumxtx-SSb0Vv6aEV3mFTw",
+    authDomain: "notificacoes-felipe.firebaseapp.com",
+    databaseURL: "https://notificacoes-felipe.firebaseio.com",
+    projectId: "notificacoes-felipe",
+    storageBucket: "notificacoes-felipe.appspot.com",
+    messagingSenderId: "510362805881"
+  };
+  firebase.initializeApp(config);
+
+/* ========================
+    Variáveis
+======================== */
+
+/* ========================
+    Eventos
+======================== */
+
+/* ========================
+    Funções 
+======================== */
+    
+}
