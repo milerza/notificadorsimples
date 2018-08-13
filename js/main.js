cce261058a4116445631261d24cf2a0392c74939
Felipe <cezarfelipe@gmail.com>
2018-08-13 10:43:38 -0300

    Aula 3

--- js/main.js
+++ js/main.js
@@ -19,19 +19,43 @@ firebase.initializeApp(config);
 ======================== */
 const FIREBASE_AUTH = firebase.auth();
 const BTN_LOGAR = document.getElementById('logar');
+const BTN_SAIR = document.getElementById('sair');
 
 /* ========================
     Eventos
 ======================== */
 BTN_LOGAR.addEventListener('click',logar);
+BTN_SAIR.addEventListener('click',sair);
+FIREBASE_AUTH.onAuthStateChanged(estadoAutenticacaoAlterado);
 
 /* ========================
     Funções 
 ======================== */
+
+function sair(){
+    FIREBASE_AUTH.signOut();
+}
+
+function estadoAutenticacaoAlterado(usuario){
+    if(usuario){
+        console.log(usuario);
+        BTN_LOGAR.setAttribute('hidden', 'true');
+        BTN_SAIR.removeAttribute('hidden');
+    } else {
+        console.log('Ninguem Logado ');
+        BTN_SAIR.setAttribute('hidden', 'true');
+        BTN_LOGAR.removeAttribute('hidden');
+    }
+}
+
 function logar(){
     FIREBASE_AUTH.signInWithPopup( 
         new firebase.auth.GoogleAuthProvider()
     )
 }
+
+
+
+
 
 }
